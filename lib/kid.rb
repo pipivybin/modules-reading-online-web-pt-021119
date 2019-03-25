cac1f10d97d2ba572ca63b77675d974684e3676b
Ming Duan <11203duan@gmail.com>
2019-03-25 19:10:14 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,9 +3,9 @@ require_relative './class_methods_module.rb'
 
 class Kid
 
-include Dance, FancyDance
+include FancyDance::InstanceMethods
 
-extend MetaDancing
+extend FancyDance::ClassMethods
 
 attr_accessor :name
 
